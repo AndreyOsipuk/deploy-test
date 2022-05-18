edde8a8122117a201ef78ca4e764233ff3232156
Andrey Osipuk <andrey.o@list.ru>
2022-05-18 21:29:44 +0300

    init npm

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const indexFile = fs.readFileSync('./index.html', { encoding: 'utf8' });
 const pageFile = fs.readFileSync('./about.html', { encoding: 'utf8' });
 
-
 http
   .createServer((request, response) => {
     if (request.url === '/') {
